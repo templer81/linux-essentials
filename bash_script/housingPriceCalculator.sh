835c697a982557d384bdc9b0b97758a0218e9095
shadowwalker <shadowwalker1117@hotmail.com>
2016-08-13 19:55:36 -0700

    finish housing price calculator

--- bash_script/housingPriceCalculator.sh
+++ bash_script/housingPriceCalculator.sh
@@ -5,6 +5,7 @@
 
 # command line usage:
 # ./housingPriceCalculator 10 5 3
+#           0              1  2 3
 # 150
 
 # if width or length is smaller or equal than 0
@@ -12,6 +13,22 @@
 
 usage(){
     # give some instructions for users here
-    :
+    echo
+    echo "This script helps you to calculate price of house."
+    echo "Usage:"
+    echo "  $0 width length pricePerUnit"
+    echo
 }
 
+if [[ $3 == "" ]]; then
+    echo "not enough parameters provided"
+    usage
+    exit 2 # not enough parameters
+fi
+
+if (( $1 > 0 && $2 > 0 )); then
+    echo "price=" $(( $1*$2*$3 ))
+else
+    echo "width or length cannot be negative or zero"
+    exit 1 # illegal parameters
+fi
