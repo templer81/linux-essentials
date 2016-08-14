a67f9f4819e939021ae89088de80d9893a906ac2
shadowwalker <shadowwalker1117@hotmail.com>
2016-08-13 19:31:22 -0700

    add housingPriceCalculator.sh

--- bash_script/clock.sh
+++ bash_script/clock.sh
@@ -2,3 +2,10 @@
 
 # design a virtual clock which will update each second
 # use while loop, date, sleep
+
+while [[ 0 ]]; do
+    # -n do not print newline
+    # -e escape
+    echo -ne `date`"\r"
+    sleep 1
+done
---
+++ bash_script/housingPriceCalculator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# calculate the price of a house
+# width*length*pricePerUnit
+
+# command line usage:
+# ./housingPriceCalculator 10 5 3
+# 150
+
+# if width or length is smaller or equal than 0
+# give an error message
+
+usage(){
+    # give some instructions for users here
+    :
+}
+
